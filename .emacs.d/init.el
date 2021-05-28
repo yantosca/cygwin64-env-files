2c795f9797828cfe87498b3fb343ac7d571d55b0
Bob Yantosca <yantosca@seas.harvard.edu>
2021-05-28 12:36:21 -0400

    emacs-config submod update: Add vterm + if statement for frame display
    We have added the compiled vterm capability to emacs-config.
    
    We also now use variable "two-vertical-frames" in ../init.el
    (outside of the emacs-config repo) to decide if we need to
    load the laptop or desktop frame options
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,9 @@
 ;;; init.el -- Initialization file for Emacs
 ;;
-;; 25 May 2021 -- Bob Yantosca -- yantosca@seas.harvard.edu
+;; 28 May 2021 -- Bob Yantosca -- yantosca@seas.harvard.edu
+
+;; Define if you want 2 vertical frames (t) or not (nil)
+(setq two-vertical-frames nil)
 
 ;; Load emacs configurations from the emacs-config submodule
 (add-to-list 'load-path "~/.emacs.d/emacs-config")
